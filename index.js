fd2bf8119f1a655734c48912119699013c1b61e6
Norrismi <Norrismi0@gmail.com>
2021-04-09 11:54:11 -0400

    working

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const questions = [
             "ShopCommerce",
             "BigCommerce"
         ],
-        //correct: 0,
         number: 1,
     },
     {
@@ -22,7 +21,6 @@ const questions = [
             "To provide support with a deep understanding of how the platform works and what its limitations are",
             "All the above"
         ],
-        //correct: 2,
         number: 2,
 
     },
@@ -34,7 +32,6 @@ const questions = [
             "They need to know web development, the platform itself, and the liquid template language",
             "All the above"
         ],
-        //correct: 2,
         number: 3,
 
     }
@@ -45,63 +42,70 @@ const questions = [
 
 
 
-let correctAnswer = 0;
 
 
 //Question One
-let questOneSelection = document.querySelectorAll('.question_one input')
+let questOneSelection = document.querySelectorAll('.question_1 input')
 questOneSelection.forEach(selection => {
     selection.addEventListener('change', e => {
        let selected = e.target.value.toLowerCase()
        let correct = questions[0].a[0].toLowerCase()
+       
 
        console.log(selected )
         if(selected == correct ){
-            correctAnswer++
+            correctAnswer += 1
+            console.log('you are correct ' + correctAnswer)
         }
     })
 })
 
 //Question Two
-let questTwoSelection = document.querySelectorAll('.question_two input')
+let questTwoSelection = document.querySelectorAll('.question_2 input')
 questTwoSelection.forEach(selection => {
     selection.addEventListener('change', e=> {
         let selected = e.target.value.toLowerCase()
         let correct = 'all the above'
+        
 
         if(selected === correct){
-            correctAnswer++
-            // console.log('you are correct ' + correctAnswer)
-        }
-    })
-})
-
-//Question Three
-let questThreeSelection = document.querySelectorAll('.question_three input')
-questThreeSelection.forEach(selection => {
-    selection.addEventListener('change', e=> {
-        let selected = e.target.value.toLowerCase()
-        let correct = 'all the above'
-
-        if(selected === correct){
-            correctAnswer++
+            correctAnswer += 1
              console.log('you are correct ' + correctAnswer)
         }
     })
 })
 
+//Question Three
+let questThreeSelection = document.querySelectorAll('.question_3 input')
+questThreeSelection.forEach(selection => {
+    selection.addEventListener('change', e=> {
+        let selected = e.target.value.toLowerCase()
+        let correct = 'all the above'
+        questionCount.innerHTML = 3
 
+        if(selected === correct){
+            correctAnswer += 1
+             console.log('you are correct ' + correctAnswer)
+        }
+    })
+})
 
+//Selectors
+let questionCount = document.querySelector('.quiz_question-count .quiz_question-count')
+let questCountContainer = document.querySelector('.quiz_question-count')
 let questionOneBtn = document.querySelector('.question_one_submit')
 let questionTwoBtn = document.querySelector('.question_two_submit')
 let questionThreeBtn = document.querySelector('.question_three_submit')
 let result = document.querySelector('.quiz_result')
 
+//Initial values
+questionCount.innerHTML = 1
+let correctAnswer = 0;
 
 
-let questionOne = document.querySelector('.question_one')
-let questionTwo = document.querySelector('.question_two')
-let questionThree = document.querySelector('.question_three')
+let questionOne = document.querySelector('.question_1')
+let questionTwo = document.querySelector('.question_2')
+let questionThree = document.querySelector('.question_3')
 
 
     //show question one
@@ -114,35 +118,38 @@ let questionThree = document.querySelector('.question_three')
     })
 
 
-
+   //show question two
     questionOneBtn.addEventListener('click', e=>{
         e.preventDefault()
         questionOne.style.display = 'none'
         questionTwo.style.display = 'block'
         questionThree.style.display = 'none'
         result.style.display = 'none'
+        questionCount.innerHTML = 2
     })
 
-
+   //show question three
     questionTwoBtn.addEventListener('click', e=>{
         e.preventDefault()
         questionOne.style.display = 'none'
         questionTwo.style.display = 'none'
         questionThree.style.display = 'block'
         result.style.display = 'none'
+        questionCount.innerHTML = 3
     })
-
-
+    
+    //show final screen
     questionThreeBtn.addEventListener('click', e=>{
         e.preventDefault()
         questionOne.style.display = 'none'
         questionTwo.style.display = 'none'
         questionThree.style.display = 'none'
+        questCountContainer.style.display = 'none'
         result.style.display = 'block'
+
+        result.innerHTML = `Thanks for taking the quiz your result is ${correctAnswer}/3 `
     })
 
-
-    result.innerHTML = `Thanks for taking the quiz your result is ${correctAnswer}/3 `
 
 
 
